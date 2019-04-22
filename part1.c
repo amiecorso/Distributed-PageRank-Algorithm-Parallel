ffba768729ecfa178a042868a72b3b2e77c4a5fd
amiecorso <acorso@cs.uoregon.edu>
2019-04-22 14:21:02 -0700

    woooo

--- part1.c
+++ part1.c
@@ -22,9 +22,16 @@ int main(int argc, char *argv[]) {
 	fclose(f_in);
 }
 */
+long MAXNODES = 5000000000;
+long MAXID = 0;
+int *COUNTS; 
+double *RECIP;
+double *CREDIT;
+int **NEIGHBS;
+int MAXNODE = 0; // largest count of neighbors
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
+    COUNTS = calloc(MAXNODES, sizeof(int));
     unsigned char *f;
     int size;
     struct stat s;
@@ -35,16 +42,65 @@ int main(int argc, char const *argv[])
     size = s.st_size;
 
     f = (char *) mmap (0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (!f) {
+        return 1;
+    }
+    char *numbuf = malloc(20); // make an array for storing characters from file
+    int index = 0; // for filling our numbuf
+    int a, b;
+    int firstspace = 1;
     for (int i = 0; i < size; i++) {
         char c;
-
         c = f[i];
-        putchar(c);
+        //putchar(c);
+        if (c == ' ') {
+            if (firstspace) {
+                numbuf[index] = '\0'; //terminate a
+                a = atoi(numbuf);     // read it as an int
+                printf("a: %i   ", a);
+                index = 0;
+                firstspace = 0;
+                continue;
+            }
+            else
+                continue;
+        }
+        if (c == '\n') {
+            numbuf[index] = '\0';
+            b = atoi(numbuf);
+            printf("b: %i   \n", b);
+            index = 0;
+            firstspace = 1;
+            // TODO: we now have a and b! do things with them (count, store neighbors)
+            COUNTS[a] += 1;
+            COUNTS[b] += 1;
+            if (a > MAXID) MAXID = a;
+            if (b > MAXID) MAXID = b;
+            continue;
+        }
+        numbuf[index] = c;
+        ++index;
+        firstspace = 1;
     }
-//    for (int i = 0; i < 100; i++) {
-//        printf("%s", f[i]);
-//    }
+    // FIND MAX NODE
+    for (int i = 0; i < MAXID; i++) {
+        if (COUNTS[i] > MAXNODE) MAXNODE = COUNTS[i];
+    } //endfor
+    printf("most neighbors: %i\n", MAXNODE);
+    // CREATE RECIPROCAL ARRAY
+    RECIP = calloc(MAXID, sizeof(int));
+    int count;
+    for (int i = 0; i < MAXID; i++) {
+        count = COUNTS[i];
+        if (count) RECIP[i] = 1 / count;
+    } //endfor
+
+    // ALLOCATE 2-D array
+    NEIGHBS = malloc(MAXID * sizeof(int *));
+    for (int i = 0; i < MAXID; i++) {
+        NEIGHBS[i] = malloc(MAXNODE * sizeof(int));
+    }
+    
 
     return 0;
 }
-

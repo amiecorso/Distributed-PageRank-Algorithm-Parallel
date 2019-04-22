0cbdc0bf70630510051aaf1a0973c0c91c55aa93
amiecorso <acorso@cs.uoregon.edu>
2019-04-22 16:20:01 -0700

    almost there

--- part1.c
+++ part1.c
@@ -10,18 +10,7 @@
 #include <sys/time.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
-/*
-int main(int argc, char *argv[]) {
-    FILE *f_in;
-	f_in = fopen(argv[1], "r");
-    if (!f_in) {
-        return 1;
-    }
-    buffer = malloc(100);
-	fread(buffer, width*height*sizeof(Pixel), 1, f_in);
-	fclose(f_in);
-}
-*/
+
 long MAXNODES = 5000000000;
 long MAXID = 0;
 int *COUNTS; 
@@ -71,7 +60,7 @@ int main(int argc, char const *argv[]) {
             printf("b: %i   \n", b);
             index = 0;
             firstspace = 1;
-            // TODO: we now have a and b! do things with them (count, store neighbors)
+            // Count number of neighbors for each node 
             COUNTS[a] += 1;
             COUNTS[b] += 1;
             if (a > MAXID) MAXID = a;
@@ -95,12 +84,84 @@ int main(int argc, char const *argv[]) {
         if (count) RECIP[i] = 1 / count;
     } //endfor
 
-    // ALLOCATE 2-D array
-    NEIGHBS = malloc(MAXID * sizeof(int *));
-    for (int i = 0; i < MAXID; i++) {
-        NEIGHBS[i] = malloc(MAXNODE * sizeof(int));
+    // INITIALIZE CREDIT
+    CREDIT = malloc((MAXID + 1) * sizeof(double));
+    for (int i = 0; i <= MAXID; i++) {
+        CREDIT[i] = 1.0; 
     }
-    
 
+    // ALLOCATE 2-D array
+    NEIGHBS = malloc((MAXID + 1) * sizeof(int *));
+    for (int i = 0; i <= MAXID; i++) {
+        NEIGHBS[i] = malloc((MAXNODE + 1) * sizeof(int));
+        NEIGHBS[i][0] = 1; // INDEX info
+    }
+
+    index = 0;
+    int nextindex;
+    for (int i = 0; i < size; i++) {
+        char c;
+        c = f[i];
+        //putchar(c);
+        if (c == ' ') {
+            if (firstspace) {
+                numbuf[index] = '\0'; //terminate a
+                a = atoi(numbuf);     // read it as an int
+                //printf("a: %i", a);
+                index = 0;
+                firstspace = 0;
+                continue;
+            }
+            else
+                continue;
+        }
+        if (c == '\n') {
+            numbuf[index] = '\0';
+            b = atoi(numbuf);
+            //printf("b: %i   \n", b);
+            index = 0;
+            firstspace = 1;
+            // handle a
+            nextindex = NEIGHBS[a][0];
+            //printf("nextindex: %i\n", nextindex);
+            NEIGHBS[a][nextindex] = b;
+            NEIGHBS[a][0] = nextindex + 1;
+            // handle b
+            nextindex = NEIGHBS[b][0];
+            NEIGHBS[b][nextindex] = a;
+            NEIGHBS[b][0] = nextindex + 1;
+            continue;
+        }
+        numbuf[index] = c;
+        ++index;
+        firstspace = 1;
+    }
+    // PERFORM ROUNDS
+    int numrounds = atoi(argv[2]);
+    double **ROUNDS = malloc(numrounds * sizeof(double *));
+    for (int i = 0; i < numrounds; i++) { // round
+        ROUNDS[i] = malloc((MAXID + 1) * sizeof(double));
+        for (int n = 0; n <= MAXID; n++) { // node
+            double newcred = 0.0; // new credit for this node, this round
+            int neighbcount = COUNTS[n];
+            if (neighbcount) { // only need to perform update if this node has neighbors - i.e. if this node ID exists
+                for (int neighindex = 1; neighindex <= neighbcount; neighindex++) {
+                    int neighbor = NEIGHBS[n][neighindex];
+                    newcred += CREDIT[neighbor] * RECIP[neighbor];
+                } //endfor neighindex
+                ROUNDS[i][n] = newcred;
+            } //endif
+        }//endfor n
+        // copy into array
+        for (int j = 0; j <= MAXID; j++) {
+            CREDIT[j] = ROUNDS[i][j];
+        } 
+    }//endfor i
+
+    // PRINT ROUND RESULTS
+
+
+    // TODO: write to file
     return 0;
+
 }

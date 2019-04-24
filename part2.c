8eff84344fd057ea47fb6ea9ebd12120a78a8ff9
Amie Corso <acorso@ix.cs.uoregon.edu>
2019-04-24 07:31:51 -0700

    asdf

--- part2.c
+++ part2.c
@@ -1,4 +1,8 @@
 // part2.c
+// TODO:
+// - error checking
+// - this partition only needs to record credit/degree/neighbors for ITS partition nodes
+// MPI stuff next
 #include <netdb.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -11,18 +15,17 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 
-//long MAXNODES = 5000000000;
-//long MAXNODES = 50;
 long MAXNODES = 10000000;
 long MAXID = 0;
-long MAXDEG = 0;
 int *COUNTS; 
+int *PART;
 double *RECIP;
 double *CREDIT;
 int **NEIGHBS;
 
 int main(int argc, char const *argv[]) {
     COUNTS = calloc(MAXNODES, sizeof(int));
+    PART = calloc(MAXNODES, sizeof(int));
 
     char *graphf = argv[1];
     char *partf = argv[2];
@@ -65,7 +68,10 @@ int main(int argc, char const *argv[]) {
             d = atoi(token);
             token = strtok(NULL, "\t\t");
             p = atoi(token);
-            printf("n: %i,  d: %i, p: %i\n", n, d, p);
+            COUNTS[n] = d;
+            PART[n] = p;
+            if (n > MAXID) MAXID = n;
+            //printf("n: %i,  d: %i, p: %i\n", n, d, p);
             index = 0;
         }//endif
         else {
@@ -74,7 +80,14 @@ int main(int argc, char const *argv[]) {
         }
     }
 
-    // READ GRAPH FILE ================
+    // ALLOCATE 2-D array ======================
+    NEIGHBS = malloc((MAXID + 1) * sizeof(int *));
+    for (int i = 0; i <= MAXID; i++) {
+        NEIGHBS[i] = malloc((COUNTS[i] + 1) * sizeof(int));
+        NEIGHBS[i][0] = 1; // INDEX info
+    }
+
+    // READ GRAPH FILE (POPULATE NEIGHBS)================
     fd = open(graphf, O_RDONLY);
         // get size of file
     status = fstat (fd, & s);
@@ -87,6 +100,7 @@ int main(int argc, char const *argv[]) {
     char *newbuf = malloc(64); // make an array for storing characters from file
     index = 0; // for filling our numbuf
     int a, b;
+    int nextindex;
     for (int i = 0; i < size; i++) {
         c = f[i];
         //putchar(c);
@@ -97,77 +111,32 @@ int main(int argc, char const *argv[]) {
             a = atoi(token);
             token = strtok(NULL, "\t\t");
             b = atoi(token);
-
-            // Count number of neighbors for each node 
-            COUNTS[a] += 1;
-            COUNTS[b] += 1;
-            if (a > MAXID) MAXID = a;
-            if (b > MAXID) MAXID = b;
-        }
-        else {
-            newbuf[index] = c;
-            ++index;
-        }
-    }
-
-    // ALLOCATE 2-D array ======================
-    NEIGHBS = malloc((MAXID + 1) * sizeof(int *));
-    for (int i = 0; i <= MAXID; i++) {
-        NEIGHBS[i] = malloc((COUNTS[i] + 1) * sizeof(int));
-        NEIGHBS[i][0] = 1; // INDEX info
-    }
-
-    // POPULATE NEIGHBOR ARRAY ====================
-    index = 0;
-    int nextindex;
-    for (int i = 0; i < size; i++) {
-        char c;
-        c = f[i];
-        //putchar(c);
-        if (c == '\t') {
-            if (firstspace) {
-                numbuf[index] = '\0'; //terminate a
-                a = atoi(numbuf);     // read it as an int
-                //printf("a: %i", a);
-                index = 0;
-                firstspace = 0;
-                continue;
-            }
-            else
-                continue;
-        }
-        if (c == '\n') {
-            numbuf[index] = '\0';
-            b = atoi(numbuf);
-            //printf("b: %i   \n", b);
-            index = 0;
-            firstspace = 1;
-            // handle a
+            //fprintf(stderr, "a: %i, b: %i\n", a, b);
+            // populate neighbor arrays
             nextindex = NEIGHBS[a][0];
-            //printf("nextindex: %i\n", nextindex);
             NEIGHBS[a][nextindex] = b;
             NEIGHBS[a][0] = nextindex + 1;
             // handle b
             nextindex = NEIGHBS[b][0];
             NEIGHBS[b][nextindex] = a;
             NEIGHBS[b][0] = nextindex + 1;
-            continue;
         }
-        numbuf[index] = c;
-        ++index;
-        firstspace = 1;
+        else {
+            newbuf[index] = c;
+            ++index;
+        }
     }
     end = clock();
     elapsed = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Time to read: %f seconds\n", elapsed);
 
-    // INITIALIZE CREDIT
+    // INITIALIZE CREDIT =================
     CREDIT = malloc((MAXID + 1) * sizeof(double));
     for (int i = 0; i <= MAXID; i++) {
         CREDIT[i] = 1.0; 
     }
 
-    // CREATE RECIPROCAL ARRAY
+    // CREATE RECIPROCAL ARRAY ============
     RECIP = calloc(MAXID + 1, sizeof(double));
     int count;
     for (int i = 0; i <= MAXID; i++) {
@@ -177,8 +146,7 @@ int main(int argc, char const *argv[]) {
         }
     } //endfor
 
-//    fprintf(stdout, "seg fault? %d\n", __LINE__);
-    // PERFORM ROUNDS
+    // PERFORM ROUNDS =====================
     int numrounds = atoi(argv[2]);
     double **ROUNDS = malloc(numrounds * sizeof(double *));
     for (int i = 0; i < numrounds; i++) { // round
